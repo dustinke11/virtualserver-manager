de119cde9d1e0095ff203b25c830235d425efc7c
Dustin Ke <dustinke@MacBook-Pro.lan>
2024-11-12 21:38:10 -0800

    update

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,7 +30,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	nginxv1 "virtualservermanager/api/v1"
+	nginxv1 "virtualserver-manager/api/v1"
 	//+kubebuilder:scaffold:imports
 )
 
